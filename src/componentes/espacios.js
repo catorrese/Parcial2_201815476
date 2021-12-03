94c2a393fad1db769ce1fcba9d2c37488911d8c5
catorrese <ca.torrese@uniandes.edu.co>
2021-12-03 13:44:48 -0500

    Avance

--- src/componentes/espacios.js
+++ src/componentes/espacios.js
@@ -1,78 +1,97 @@
 import React, { useEffect, useState } from "react";
-import casa from './imagenes/casa.jpg';
-import apto from './imagenes/apto.jpg';
+import casa from "./imagenes/casa.jpg";
+import apto from "./imagenes/apto.jpg";
+import living from "./imagenes/living.jpg";
 
 function Espacios() {
+  const url =
+    "https://gist.githubusercontent.com/josejbocanegra/0067d2b28b009140fee423cfc84e40e6/raw/6e6b11160fbcacb56621b6422684d615dc3a0d33/spaces.json";
+  const [espacios, setEspacios] = useState([]);
 
-    const url = "https://gist.githubusercontent.com/josejbocanegra/0067d2b28b009140fee423cfc84e40e6/raw/6e6b11160fbcacb56621b6422684d615dc3a0d33/spaces.json";
-    const [espacios, setEspacios] = useState([]);
-    useEffect(() => {
-        const requestOptions = {
-            method: "GET",
+  const url2 = "https://gist.githubusercontent.com/josejbocanegra/92c90d5f2171739bd4a76d639f1271ea/raw/9effd124c825f7c2a7087d4a50fa4a91c5d34558/rooms.json";
+  const [rooms, setRooms] = useState([]);
 
-        };
-        fetch(url, requestOptions)
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+    };
+    fetch(url, requestOptions)
+      .then((res) => res.json())
+      .then((data) => {
+       
+        setEspacios(data);
+      })
+      .catch();
+  }, []);
+
+
+  function loadRooms(id){
+    const requestOptions = {
+        method: "GET",
+      };
+      fetch(url2, requestOptions)
         .then((res) => res.json())
-          .then((data) => {
-            console.log(data);
-            setEspacios(data);
-          })
-          .catch();
-    },[])
+        .then((data) => {
+          let data2=[]
+          data.forEach(element => {
+              if(element.homeId == id){
+                  data2.push(element)
+              }
+          });
+          setRooms(data2);
+        })
+        .catch();
+  }
 
-    const renderEspacios = (espacios, index) => {
-        let tipo = apto
-        if (espacios.type == "house"){
-            tipo = casa;
-        }
-        
-    
-        return (
-          /* <div id="cardRegistrar" className="card">
-            <div className="container row">
-              <div className="col">
-                <img
-                  scr="https://via.placeholder.com/150"
-                  className="rounded float-left"
-                  alt=""
-                />
-              </div>
-              <div className="col">
-                <h4>{viajes.direccion}</h4>
-                <h5>Precio: $ {viajes.precio}</h5>
-                <h5>Fecha salida: {viajes.fecha}</h5>
-              </div>
-              <div className="col justify-content-center">
-                <button type="submit" className="btn-primary btn-block btn-lg">
-                  Reservar
-                </button>
-              </div>
-            </div>
-          </div> */
+  const renderEspacios = (espacios, index) => {
+    let tipo = apto;
+    if (espacios.type == "house") {
+      tipo = casa;
+    }
 
-    <div class="col-sm-3">
+    return (
+      <div class="col-sm-3">
         <div className="card">
           <img src={tipo} class="card-img-top" />
           <div className="card-body">
             <h4 className="card-title">{espacios.name} </h4>
             <p class="card-text">{espacios.address} </p>
+            <a  class="stretched-link" onClick={() => {loadRooms(espacios.id)}}></a>
             <br />
           </div>
-          </div>
+        </div>
       </div>
+    );
+  };
 
-        );
-      };
+  const renderRooms = (rooms, index) => {
+
+    let tipo = living;
+    
 
     return (
-    <div>
-    <h3 >My spaces</h3>
-    <div className="row Cards">
-    {espacios.map(renderEspacios)}
+      <div class="col-sm-3">
+        <div className="card">
+          <img src={tipo} class="card-img-top" />
+          <div className="card-body">
+            <h4 className="card-title">{rooms.name} </h4>
+            <p class="card-text">{rooms.address} </p>
+            <a href="#" class="stretched-link"></a>
+            <br />
+          </div>
+        </div>
       </div>
-      </div>
-   
     );
+  };
+
+  return (
+    <div>
+      <h3>My spaces</h3>
+      <div className="row Cards">{espacios.map(renderEspacios)}</div>
+      <h3>My Rooms</h3>
+      <div className="row Cards">{rooms.map(renderRooms)}</div>
+    </div>
+  );
 }
 
 export default Espacios;
